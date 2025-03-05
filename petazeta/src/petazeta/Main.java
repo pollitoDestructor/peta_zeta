477cf434e181c4b014ef78253f95b3bebd71d9a8
Run06 <Socramnita6@gmail.com>
2025-03-05 21:14:39 +0100

    Poner bomba con el espacio

--- petazeta/src/petazeta/Main.java
+++ petazeta/src/petazeta/Main.java
@@ -7,6 +7,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		
 		Jugador jugador = Jugador.getJugador();
 		Tablero tablero = Tablero.getTablero();
 		JFrame f = new JFrame();
@@ -14,7 +15,7 @@ public class Main {
 		f.addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent e) {
 				int keyCode=e.getKeyCode();
-				System.out.println(keyCode);
+				//System.out.println(keyCode);
 				if(keyCode == KeyEvent.VK_W) { //Movimiento arriba
 					jugador.mover(jugador.getPosX(), jugador.getPosY()-1);
 					System.out.println("Se mueve arriba");
@@ -31,14 +32,21 @@ public class Main {
 					jugador.mover(jugador.getPosX()+1, jugador.getPosY());
 					System.out.println("Se mueve a la derecha");
 				}
+				else if(keyCode == KeyEvent.VK_SPACE) { //Poner bomba
+					Bomba bomba = new Bomba(jugador.getPosX(),jugador.getPosY());
+					System.out.println("Se pone una bomba");
+				}
 				
+				tablero.printMap();
 				System.out.println("El jugador está en: "+jugador.getPosX()+" "+jugador.getPosY());
+				
 			}
 		});
 		
 		tablero.printMap();
 		System.out.println("El jugador está en: "+jugador.getPosX()+" "+jugador.getPosY());
 		f.setVisible(true);
+		
 	}
 
 }

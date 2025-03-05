374bed9833b87cd54ff746784a99eb402f2b5c7e
Run06 <Socramnita6@gmail.com>
2025-03-05 18:14:22 +0100

    Funciones de movimiento del jugador, aún no funcionan.

--- petazeta/src/petazeta/Main.java
+++ petazeta/src/petazeta/Main.java
@@ -1,48 +1,38 @@
 package petazeta;
 
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import javax.swing.JFrame;
 
-public class Main implements KeyListener {
+public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Jugador jugador = Jugador.getJugador();
 		Tablero tablero = Tablero.getTablero();
-		
-		tablero.printMap();
-		System.out.println("El jugador está en: "+jugador.getPosX()+" "+jugador.getPosY());
-		prueba.main(args);
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		Jugador jugador = Jugador.getJugador();
-		if(e.getKeyCode() == KeyEvent.VK_W) { //Movimiento arriba
-			jugador.mover(jugador.getPosX(), jugador.getPosY()+1);
-		}
-		else if(e.getKeyCode() == KeyEvent.VK_S) { //Movimiento abajo
-			jugador.mover(jugador.getPosX(), jugador.getPosY()-1);
-		}
-		else if(e.getKeyCode() == KeyEvent.VK_A) { //Movimiento izquierda
-			jugador.mover(jugador.getPosX()-1, jugador.getPosY());
-		}
-		else if(e.getKeyCode() == KeyEvent.VK_D) { //Movimiento derecha
-			jugador.mover(jugador.getPosX()+1, jugador.getPosY());
-		}
-		System.out.println("El jugador está en: "+jugador.getPosX()+" "+jugador.getPosY());
-
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		JFrame f = new JFrame();
+				
+		f.addKeyListener(new KeyAdapter() {
+			public void KeyPressed(KeyEvent e) {
+				int keyCode=e.getKeyCode();
+				System.out.println(keyCode);
+				if(e.getKeyCode() == KeyEvent.VK_W) { //Movimiento arriba
+					jugador.mover(jugador.getPosX(), jugador.getPosY()+1);
+				}
+				else if(e.getKeyCode() == KeyEvent.VK_S) { //Movimiento abajo
+					jugador.mover(jugador.getPosX(), jugador.getPosY()-1);
+				}
+				else if(e.getKeyCode() == KeyEvent.VK_A) { //Movimiento izquierda
+					jugador.mover(jugador.getPosX()-1, jugador.getPosY());
+				}
+				else if(e.getKeyCode() == KeyEvent.VK_D) { //Movimiento derecha
+					jugador.mover(jugador.getPosX()+1, jugador.getPosY());
+				}
+				System.out.println("El jugador está en: "+jugador.getPosX()+" "+jugador.getPosY());
+			}
+		});
+				tablero.printMap();
+				System.out.println("El jugador está en: "+jugador.getPosX()+" "+jugador.getPosY());
+				f.setVisible(true);
 	}
 
 }

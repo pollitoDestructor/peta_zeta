56d5a2c9cfae292ded39909cea79fad7e8a04ce8
Shaman-Alonso <shaman.alonso@gmail.com>
2025-03-05 17:51:06 +0100

    Bomba actualizada.

--- petazeta/src/petazeta/Bomba.java
+++ petazeta/src/petazeta/Bomba.java
@@ -5,43 +5,31 @@ import java.util.TimerTask;
 
 public class Bomba extends Casilla {
     private Timer timer = null;
-    private static final int PERIODO = 3; // Explota cada 3 segundos
-    private Tablero tablero; // Referencia al tablero para acceder a las casillas ,
-    						//esto es para despues para el metodo destruir linea
+    private static final int PERIODO = 3; //Periodo de 3 segundos
+    private Tablero tablero; //Referencia al tablero (constructora)
 
-    public Bomba(int a, int b, boolean c, Tablero tablero) {
-        super(a, b, c); 
-        this.tablero = tablero;
+    public Bomba(int a, int b) {
+        super(a, b, true); 
+        this.tablero = Tablero.getTablero(); //Puntero a Tablero (por comodidad)
         
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
-                explotar();
+                explotar(); //Pasados 3 segundos, explota
             }       
         };
         timer = new Timer();
-        timer.schedule(timerTask, PERIODO * 1000); // Explota después de 3 segundos
+        timer.schedule(timerTask, PERIODO * 1000); // Explota despues de 3 segundos
     }
     
-    public void explotar() {
-        // Aqui destruimos la bomba xd
-        destruir();
+    protected void explotar() {
+        destruir(); // Aqui destruimos la bomba
         
-        //Aqui se situa en los bloques de alrededor de la bomba 
-        destruirLinea(1, 0);  // Derecha (aumenta la x)
-        destruirLinea(-1, 0); // Izquierda (dism la x)
-        destruirLinea(0, 1);  // Abajo (dismin la y)
-        destruirLinea(0, -1); // Arriba (aumenta la y)
+        tablero.detonarBomba(coordX,coordY,tipoCasilla()); //Pasa coords y nombre(para herencia)
     }
     
-    private void destruirLinea(int dirX, int dirY) {
-        int x = coordX;
-        int y = coordY;
-        // Esto se puede hacer de otra manera es decir hacer los calculos desde tablero
-        while (tablero.esCasillaDestructible(x, y)) { // FALTA ESTE METODO
-            tablero.obtenerCasilla(x, y).destruir(); // FALTA ESTE METODO
-            x += dirX;
-            y += dirY;
-        }
-    }
+    public void destruir() //Destruye la Bomba
+	{
+		System.out.println("Bomba "+coordX+", "+coordY+" destruida."); 
+	}
 }

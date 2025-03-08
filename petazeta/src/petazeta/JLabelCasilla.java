cc2c5394d07936338c0d8655577316a663ff461d
gabri <gabri@LAPTOP-SUKM0I0A>
2025-03-08 09:23:28 +0100

    Corrección del patrón Observer y actualización del vista del mapa al detonar bombas.

--- petazeta/src/petazeta/JLabelCasilla.java
+++ petazeta/src/petazeta/JLabelCasilla.java
@@ -1,8 +1,12 @@
 package petazeta;
 
+import java.awt.Color;
+import java.util.Observable;
+import java.util.Observer;
+
 import javax.swing.JLabel;
 
-public class JLabelCasilla extends JLabel {
+public class JLabelCasilla extends JLabel implements Observer{
 
 	private int cX;
 	private int cY;
@@ -11,6 +15,7 @@ public class JLabelCasilla extends JLabel {
 		super();
 		cX = pCX;
 		cY = pCY;
+		Tablero.getTablero().addObserver(this);
 	}
 	
 	public int getCX() {
@@ -18,6 +23,35 @@ public class JLabelCasilla extends JLabel {
 	}
 	public int getCY() {
 		return cY;
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		//La idea es que cada vez que tablero hace una casilla llama al update diciendo que tipo
+		System.out.println("update");
+		if(o instanceof Tablero) {
+			Object[] param = (Object[])arg; //[accion:String, pX:int, pY:int, tipoBloque:String]
+			int y = 10-(int)param[2];
+			int x = (int)param[1];
+			if ((int)param[0]==0&&y==cY&&x==cX) { //comprueba la accion que se quiere realizar (poner una casilla, detonar una bomba, mostrar el mapa, etc.)
+				System.out.println("switch");
+				System.out.println((int)param[3]);
+				switch ((int)param[3]) {
+				case 0:
+					setBackground(new Color(252, 208, 126)); //Casilla vacia
+					break;
+				case 1:
+					setBackground(new Color(245, 115, 59)); //Bloque Blando
+					break;
+				case 2:
+					setBackground(new Color(94, 16, 4)); //Bloque Duro
+					break;
+				case 3:
+					setBackground(new Color(0,0,0)); //Bomba
+					break;
+				}
+			}
+		}
 	}
 
 }
--- petazeta/src/petazeta/TableroVisual.java
+++ petazeta/src/petazeta/TableroVisual.java
@@ -20,9 +20,7 @@ import java.awt.CardLayout;
 public class TableroVisual extends JFrame implements Observer{
 
 	private static final long serialVersionUID = -1526416068663302084L;
-	private JPanel contentPane;
 	private JPanel grid;
-	private JLabel[][] mapa;
 
 
 
@@ -30,7 +28,6 @@ public class TableroVisual extends JFrame implements Observer{
 	 * Create the frame.
 	 */
 	public TableroVisual() {
-		mapa = new JLabel[17][11]; //NOTA: las matrices funcionan mediante Object[y][x]
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 850, 550);
 		JPanel contentPane = new JPanel();
@@ -43,14 +40,15 @@ public class TableroVisual extends JFrame implements Observer{
 		contentPane.add(grid);
 		grid.setLayout(new GridLayout(11, 17, 0, 0));
 		
-//		mapa = new JLabel[17][11]; //NOTA: las matrices funcionan mediante Object[y][x]
-//		setLayout(new GridLayout(11,17));
-//		for(int m = 0; m < 17; m++) {
-//			for(int n = 0; n < 11; n++) {
-//			   mapa[m][n] = new JPanel();
-//			   add(mapa[m][n]);
-//			}
-//		}
+		JLabelCasilla labelCasilla;
+		for(int m = 0; m < 17; m++) {
+			for(int n = 10; n >= 0; n--) {
+				labelCasilla = new JLabelCasilla(m,n);
+				labelCasilla.setBackground(new Color(0,0,255));
+				labelCasilla.setOpaque(true);
+				grid.add(labelCasilla,n,m);
+			}
+		}
 		
 		Tablero.getTablero().addObserver(this);
 	}
@@ -60,78 +58,8 @@ public class TableroVisual extends JFrame implements Observer{
 		//La idea es que cada vez que tablero hace una casilla llama al update diciendo que tipo
 		System.out.println("update");
 		if(o instanceof Tablero) {
-			JLabelCasilla labelCasilla;
-			JLabelBBlando labelBBlando;
-			JLabelBDuro labelBDuro;
-			JLabelBomba labelBomba;
-			
-			System.out.println("tab");
-			Object[] param = (Object[])arg; //[accion:String, pX:int, pY:int, tipoBloque:String]
-			int y = (int)param[2];
-			int x = (int)param[1];
-			int yInv = 11-y;		//para que se imprima con la orientacion correcta; no funciona :/
-			System.out.println((int)param[0]);
-			if ((int)param[0]==0) { //comprueba la accion que se quiere realizar (poner una casilla, detonar una bomba, mostrar el mapa, etc.)
-				System.out.println("switch");
-				System.out.println((int)param[3]);
-				switch ((int)param[3]) {
-				case 0:
-					System.out.println("casilla intento");
-					
-					labelCasilla = new JLabelCasilla((int)param[1],(int)param[2]);
-					labelCasilla.setBackground(new Color(255,255,255));
-					labelCasilla.setOpaque(true);
-					mapa[(int)param[1]][(int)param[2]] = labelCasilla;
-					grid.add(labelCasilla,yInv,x);
-					System.out.println("casilla puesta");
-					break;
-				case 1:
-					
-					labelBBlando = new JLabelBBlando((int)param[1],(int)param[2]);
-					labelBBlando.setBackground(new Color(255,100,100));
-					labelBBlando.setOpaque(true);
-					mapa[(int)param[1]][(int)param[2]] = labelBBlando;
-					grid.add(labelBBlando,yInv,x);
-					break;
-				case 2:
-					
-					labelBDuro = new JLabelBDuro((int)param[1],(int)param[2]);
-					labelBDuro.setBackground(new Color(0,0,0));
-					labelBDuro.setOpaque(true);
-					mapa[(int)param[1]][(int)param[2]] = labelBDuro;
-					grid.add(labelBDuro,yInv,x);
-					break;
-				case 3:
-					labelBomba = new JLabelBomba((int)param[1],(int)param[2]);
-					labelBomba.setBackground(new Color(0,0,0));
-					labelBomba.setOpaque(true);
-					mapa[(int)param[1]][(int)param[2]] = labelBomba;
-					grid.add(labelBomba,yInv,x);
-					break;
-				}
-			} 
-//			else if((int)param[0]==2){
-//				JPanel contentPane = new JPanel();
-//				contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-//
-//				setContentPane(contentPane);
-//				contentPane.setLayout(new BorderLayout(0, 0));
-//				
-//				grid = new JPanel();
-//				contentPane.add(grid, BorderLayout.CENTER);
-//				grid.setLayout(new GridLayout(11, 17, 0, 0));
-//				
-//				for(int i = 0;i<17;i++) {
-//					for(int j = 0;j<11;j++) {
-//						System.out.println("act mapa");
-////						if(mapa[i][j] != null) {
-//							grid.add(mapa[i][j],10-j,i);
-////						} 
-//					}
-//				}
-//			}
-//			grid.validate();
-//			grid.repaint();
+			grid.validate();
+			grid.repaint();
 		}
 	}
 }

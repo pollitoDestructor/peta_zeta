c883392dfe017f82c7281a23bdc6dd178ab7ce44
xehelex <diezortuzaralexander@gmail.com>
2025-03-08 10:46:30 +0100

    Para que se vea la bomba pero falla , solo se ve la explosión.

--- petazeta/src/petazeta/JLabelBomba.java
+++ petazeta/src/petazeta/JLabelBomba.java
@@ -1,23 +1,25 @@
 package petazeta;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class JLabelBomba extends JLabel {
 
-	private int cX;
-	private int cY;
-	
-	public JLabelBomba(int pCX, int pCY) {
-		super();
-		cX = pCX;
-		cY = pCY;
-	}
-	
-	public int getCX() {
-		return cX;
-	}
-	public int getCY() {
-		return cY;
-	}
+    private int cX;
+    private int cY;
 
+    public JLabelBomba(int pCX, int pCY, ImageIcon icon) {
+        super(icon); // Llama al constructor de JLabel con el ícono
+        cX = pCX;
+        cY = pCY;
+    }
+
+
+    public int getCX() {
+        return cX;
+    }
+
+    public int getCY() {
+        return cY;
+    }
 }
---
+++ petazeta/src/petazeta/ViewBomba.java
@@ -0,0 +1,38 @@
+package petazeta;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ViewBomba extends JPanel {
+    private static final long serialVersionUID = 1L;
+    private JLabelBomba lblBomba;
+    private Bomba bomba;
+
+    public ViewBomba(int x, int y) {
+        this.bomba = new Bomba(x, y);
+        inicializar();
+    }
+
+    private void inicializar() //INICIALIZAMOS LA IMAGEN DE LA BOMBA
+    {
+        this.setLayout(new GridBagLayout());
+        lblBomba = new JLabelBomba(5, 5, new ImageIcon(getClass().getResource("bomb1.png")));
+        this.add(lblBomba);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                explotar();
+            }
+        }, 3000); // EXPLOTA
+    }
+
+    private void explotar() //AQUI AL EXPLOTAR CAMBIAMOS AL GIF (solo en la bomba)
+    {
+        lblBomba.setIcon(new ImageIcon(getClass().getResource("blast.gif")));
+        bomba.destruir();
+    }
+}

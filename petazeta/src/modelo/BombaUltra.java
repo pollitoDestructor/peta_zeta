73c2090b7bd30c70a81503ab37132b3a2279c606
Shaman-Alonso <shaman.alonso@gmail.com>
2025-04-03 10:38:35 +0200

    Bug arreglado, ahora el State.

--- petazeta/src/modelo/BombaUltra.java
+++ petazeta/src/modelo/BombaUltra.java
@@ -4,18 +4,9 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class BombaUltra extends Bomba {
-    private static final int PERIODO = 3; // Explota después de 3 segundos
+    private static final int PERIODO = 5; // Explota después de 3 segundos
 
     public BombaUltra(int a, int b) {
         super(a, b); // Llama al constructor de la clase base Bomba
-        TimerTask timerTask = new TimerTask() {
-            @Override
-            public void run() {
-                explotar(); 
-            }
-        };
-
-        Timer timer = new Timer();
-        timer.schedule(timerTask, PERIODO * 1000); //3 segundos
     }
 }
